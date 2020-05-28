c04b0e40f128222b0ff170f520d0be0dafbe363f
Lumy Kelvin <lunox273@gmail.com>
2020-05-28 11:19:45 +0900

    fix gradle

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,6 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("아아아아ㅏ졸려어어어");
+        System.out.println("Hello Java");
+        System.out.println("Hello Jenkins");
     }
 }
