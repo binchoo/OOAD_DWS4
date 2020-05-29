801f4fb941ca33c92bf7a8d54acc1cab85f750c8
binchoo <079111w@gmail.com>
2020-05-29 21:50:44 +0900

    Test Coverage Analysis Is Enabled.

--- src/main/java/org/ooad_dws4/Application.java
+++ src/main/java/org/ooad_dws4/Application.java
@@ -3,6 +3,7 @@ package org.ooad_dws4;
 public class Application {
     public static void main(String[] args) {
         TimeRunner t = new TimeRunner();
+
         Clock c = new Clock(1000, (PulseMaker) t);
         c.run();
     }
