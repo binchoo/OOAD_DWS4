d798cd9b1ee58b47b6c46e1d75aa356531eab8b8
Lumy Kelvin <lunox273@gmail.com>
2020-06-06 00:39:18 +0900

    make test case
    EventScheduler
    ModeManager
    TimeRunner

--- src/test/java/org/ooad_dws4/TimeRunnerTest.java
+++ src/test/java/org/ooad_dws4/TimeRunnerTest.java
@@ -2,19 +2,21 @@ package org.ooad_dws4;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TimeRunnerTest {
 
     @Test
-    void linkObject() {
-    }
-
-    @Test
     void systemTimeUpdate() {
-    }
-
-    @Test
-    void makePulse() {
+        TimeRunner tr = new TimeRunner();
+        HashMap<String, String> arg = new HashMap<>();
+        arg.put("newTime", "1023423");
+        tr.systemTimeUpdate(new Message(21, "updateSystemTime", arg));
+        assertEquals(1023423, tr.getSystemTime());
+        arg.put("newTime", "-23423");
+        tr.systemTimeUpdate(new Message(21, "updateWorldTime", arg));
+        assertEquals(1000000, tr.getSystemTime());
     }
 }
